efb7f81708c552fb1eab6bf08ffb9cf04a06dedc
milinsky <mail@milinsky.ru>
2018-12-14 18:40:56 +1000

    Правки

--- DependencyInjection/ContainerInterface.php
+++ DependencyInjection/ContainerInterface.php
@@ -7,10 +7,10 @@ interface ContainerInterface
 {
 
     // Возвращает объект из контейнера по $class_name
-    public function get($class_name);
+    public function get(string $class_name);
 
     // Проверяет есть ли в контейнере объект $class_name
-    public function has(string $class_name);
+    public function has(string $class_name) : bool;
     
     // Возвращает новый объекьт
     public function instance(string $class_name, array $params, bool $singletone);
